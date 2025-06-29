c45d81aa485033e3af53d562cebcee160ce6de28
sheiza-jagemi <sheiza.jagemi@student.moringaschool.com>
2025-06-29 11:14:40 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the main#main element
+const main = document.getElementById('main');
+main.remove();
+
+// Create the newHeader h1 element
+const newHeader = document.createElement('h1');
+
+// Set the id of the newHeader
+newHeader.id = 'victory';
+
+// Set the text content of the newHeader
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your name
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
